983645c65dd3233c9cb7a7ee126fa8185f74bb94
Valar <azgrhl@gmail.com>
2023-04-22 20:18:23 +0800

    調整廣告大小

--- resources/views/layouts/topads.blade.php
+++ resources/views/layouts/topads.blade.php
@@ -1,13 +1,15 @@
-<div>
-    <!-- 橫幅廣告 -->
-    <ins class="adsbygoogle"
-        style="display:block;height:90px;"
-        data-ad-client="ca-pub-9868429025931364"
-        data-ad-slot="8542942724"
-        data-ad-format="auto"
-        data-full-width-responsive="true"></ins>
-    <script>
-        (adsbygoogle = window.adsbygoogle || []).push({});
-    </script>
-    <br>
+<div style="padding: 10px;">
+    <div>
+        <!-- 橫幅廣告 -->
+        <ins class="adsbygoogle"
+            style="display:block;height:90px;"
+            data-ad-client="ca-pub-9868429025931364"
+            data-ad-slot="8542942724"
+            data-ad-format="auto"
+            data-full-width-responsive="true"></ins>
+        <script>
+            (adsbygoogle = window.adsbygoogle || []).push({});
+        </script>
+        <br>
+    </div>
 </div>
