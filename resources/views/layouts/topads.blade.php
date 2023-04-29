1deec22b73054760db7e78f201ad3497ba687665
Valar <azgrhl@gmail.com>
2023-04-29 11:59:54 +0800

    調整廣告

--- resources/views/layouts/topads.blade.php
+++ resources/views/layouts/topads.blade.php
@@ -1,9 +1,7 @@
+<!-- 橫幅廣告 -->
 <div class="topads">
-    <!-- 橫幅廣告 -->
-    <ins class="adsbygoogle"
-        style="display:block;min-width:400px;max-width:970px;width:100%;height:90px"
-        data-ad-client="ca-pub-9868429025931364"
-        data-ad-slot="8542942724"></ins>
+    <ins class="adsbygoogle" style="display:block;min-width:360px;max-width:970px;width:100%;height:90px"
+        data-ad-client="ca-pub-9868429025931364" data-ad-slot="8542942724"></ins>
     <script>
         (adsbygoogle = window.adsbygoogle || []).push({});
     </script>
