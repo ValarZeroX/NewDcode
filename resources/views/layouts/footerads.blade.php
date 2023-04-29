341afadfb412ecda6616310ced6689deea65aa52
Valar <azgrhl@gmail.com>
2023-04-29 12:02:44 +0800

    調整廣告

--- resources/views/layouts/footerads.blade.php
+++ resources/views/layouts/footerads.blade.php
@@ -1,6 +1,6 @@
 <!-- 底部廣告 -->
 <div class="topads">
-    <ins class="adsbygoogle" style="display:block;min-width:360px;max-width:970px;width:100%;height:90px;" data-ad-client="ca-pub-9868429025931364" data-ad-slot="8486090646"></ins>
+    <ins class="adsbygoogle" style="display:block;min-width:360px;max-width:970px;width:100%;height:90px;margin: 0px auto;" data-ad-client="ca-pub-9868429025931364" data-ad-slot="8486090646"></ins>
     <script>
         (adsbygoogle = window.adsbygoogle || []).push({});
     </script>
