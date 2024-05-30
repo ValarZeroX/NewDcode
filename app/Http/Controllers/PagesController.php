25f87d049bc1542c07e61001b27351d9e680eece
valar <azgrhl@gmail.com>
2024-05-30 21:23:09 +0800

    調整sitemap產生

--- app/Http/Controllers/PagesController.php
+++ app/Http/Controllers/PagesController.php
@@ -9,9 +9,32 @@ class PagesController extends Controller
 {
     public function makeSitemap()
     {
+
+        // 要排除的路径
+        $excludedPaths = [
+            '/tarot/sharetarot/',
+            '/language/en/tarot/sharetarot/',
+            '/language/es/tarot/sharetarot/',
+            '/language/fr/tarot/sharetarot/',
+            '/language/ja/tarot/sharetarot/',
+            '/language/zh-hant/tarot/sharetarot/',
+            '/language/zh-hants/tarot/sharetarot/',
+        ];
+
         //set_time_limit(300);
         $path = public_path('sitemap.xml');
-        SitemapGenerator::create(config('app.url'))->writeToFile($path);
+        SitemapGenerator::create(config('app.url'))
+            ->hasCrawled(function (Url $url) use ($excludedPaths) {
+                // 排除指定的路径
+                foreach ($excludedPaths as $excludedPath) {
+                    if (strpos($url->url, $excludedPath) !== false) {
+                        return;
+                    }
+                }
+
+                return $url;
+            })
+            ->writeToFile($path);
     }
 
     public function sitemap()
