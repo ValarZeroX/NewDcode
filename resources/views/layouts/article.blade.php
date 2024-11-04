c5dc38531d7af1c692d207eb5f6d4027d325116f
Valar <azgrhl@gmail.com>
2024-11-05 00:28:26 +0800

    修正CLS

--- resources/views/layouts/article.blade.php
+++ resources/views/layouts/article.blade.php
@@ -1,9 +1,12 @@
-<ins class="adsbygoogle"
-    style="display:block; text-align:center;"
-    data-ad-layout="in-article"
-    data-ad-format="fluid"
-    data-ad-client="ca-pub-9868429025931364"
-    data-ad-slot="7186146218"></ins>
-<script>
-    (adsbygoogle = window.adsbygoogle || []).push({});
-</script>
+<div class="topads" style="position: relative; min-height: 250px;">
+<div class="ad-placeholder"></div>
+    <ins class="adsbygoogle"
+        style="display:block; text-align:center;"
+        data-ad-layout="in-article"
+        data-ad-format="fluid"
+        data-ad-client="ca-pub-9868429025931364"
+        data-ad-slot="7186146218"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+</div>
--- resources/views/layouts/topads.blade.php
+++ resources/views/layouts/topads.blade.php
@@ -1,5 +1,6 @@
 <!-- 橫幅廣告 -->
-<div class="topads">
+<div class="topads" style="position: relative; min-height: 250px;">
+    <div class="ad-placeholder"></div>
     <ins class="adsbygoogle"
      style="display:block"
      data-ad-client="ca-pub-9868429025931364"
